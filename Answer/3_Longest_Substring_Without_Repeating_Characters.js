8958970a469d1b2e9a529c0cc52647a4e1af4a2b
Tactful-biao <1208339113@qq.com>
2019-09-12 23:09:29 +0800

    add answer to the question 3 Longest Substring Without Repeating Characters

---
+++ Answer/3_Longest_Substring_Without_Repeating_Characters.js
@@ -0,0 +1,78 @@
+/*
+Given a string, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+Input: "abcabcbb"
+Output: 3 
+Explanation: The answer is "abc", with the length of 3. 
+Example 2:
+
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3. 
+             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+// 方法一 
+// 不推荐，当字符串十分长的时候，效率极低。
+var lengthOfLongestSubstring = function(s) {
+    let 
+    	str = '',
+    	temp = '';
+    for (let i in s){
+    	let substring = s.slice(i, )
+    	for (let j in substring) {
+    		if (str.indexOf(substring[j]) === -1) {
+    			str += substring[j];
+    		} else {
+    			break;
+    		}
+    	}
+    	str.length >= temp.length ? temp = str : temp = temp;
+    	str = '';
+    }
+    return temp.length;
+
+};
+
+// 方法二 
+// 速度较快
+// 通过循环遍历每一个字符
+// 把字符添加到maxSring上
+// 如果发现重复字符
+// 记录下重复字符在maxString中出现的索引位置
+// 把从重复字符开始到当前位置i的所有字符赋值给maxString
+// 判断maxString的长度和maxLength谁大，把大的赋值给maxString
+// 继续查找，重复上面步骤
+var lengthOfLongestSubstring2 = function(s) {
+	let maxString = '';
+	let maxLength = 0;
+	let lastMatchedIndex = 0;
+	for(let i=0; i<s.length; i++){
+		const matchedIndex = maxString.indexOf(s[i]);
+		maxString = maxString + s[i];
+		if(matchedIndex >= 0){
+			const lastMatchedIndex = s.substring(0, i-1).lastIndexOf(s[i]);
+			maxString = (s[i] === s[i-1] ? s.substring(i-1, i) : s.substring(lastMatchedIndex+1, i+1));
+		}
+		if(maxString.length > maxLength){
+			maxLength = maxString.length;
+		}
+	}
+	return maxLength;
+};
+
+let str = ' ';
+
+lengthOfLongestSubstring2(str);
